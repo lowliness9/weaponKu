dea77f8d4ab4fa738b5fcd4629a9143f0c0c420b
lowliness9 <41384881+lowliness9@users.noreply.github.com>
2020-06-18 10:38:31 +0800

    Add files via upload

---
+++ proxyScan3/proxyScan3.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import requests
+import traceback
+import sys
+import argparse
+import time
+
+requests.packages.urllib3.disable_warnings()
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36',
+    'Connection':'close'
+}
+
+args = dict()
+
+sList = []
+
+outputNamer = str(time.time()) + '.txt'
+
+def cmdLineParser():
+    parser = argparse.ArgumentParser(description='proxyScan3 verify that the agent can access google.com',
+                                     usage='python proxyScan3.py --file ip.txt --type https,socks5',
+                                     add_help=False)
+
+    useage = parser.add_argument_group('Usage')
+
+    useage.add_argument('--file', metavar='file', dest="targetFile", type=str, default='',
+                        help='Load targets from targetFile (e.g. ip.txt format: 127.0.0.1:1080)')
+    useage.add_argument('--t', metavar='num', dest="threadNum", type=int, default=30,
+                        help='num of threads/concurrent, 30 by default')
+    useage.add_argument('--type', metavar='http', dest="type", type=str, default='https',
+                        help='Proxy type,https default (e.g https or https,http,socks5)')
+    useage.add_argument('-h', '--help', action='help',
+                        help='show this help message and exit')
+
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    args.update(vars(parser.parse_args()))
+
+
+def output(content):
+    lock.acquire()
+    with open(outputNamer,'a+') as f:
+        f.write(content+'\n')
+    lock.release()
+
+def sockConnect(address):
+    address = address.strip()
+    print('[-] Test {}'.format(address))
+    proxies = {"http": "{}".format(address), "https": "{}".format(address)}
+    try:
+        request = requests.get('https://www.google.com/', proxies=proxies,timeout=8,verify=False,headers=headers)
+        print(request.text)
+        if request.status_code == 200 and '<title>Google</title>' in request.text:
+            print('[+] ok {}'.format(address))
+            output(address)
+    except:
+        traceback.print_exc(file=open('error.log','a+'))
+
+def Builder():
+    ipList = open(args['targetFile'],'r+').readlines()
+    protocols = args['type'].split(',')
+    for protocol in protocols:
+        for ip in ipList:
+            sList.append('{}://{}'.format(protocol.strip(),ip.strip()))
+
+if __name__ == '__main__':
+    cmdLineParser()
+    Builder()
+    print(len(sList))
+    lock = threading.Lock()
+    with ThreadPoolExecutor(args['threadNum']) as executor:
+        executor.map(sockConnect,sList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
