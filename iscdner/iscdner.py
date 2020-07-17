967e2597cfb3a11f0531fb1939ebe875fade2244
lowliness9 <lowliness9@163.com>
2020-07-18 00:16:56 +0800

    fix bugs

--- iscdner/iscdner.py
+++ iscdner/iscdner.py
@@ -2,6 +2,19 @@
 import dns.resolver
 import os
 import sys
+import re
+
+
+limit = 2
+limitIP = []
+
+
+def isIP(ip):
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    if p.match(ip):
+        return True
+    else:
+        return False
 
 def dnsQuery(site):
     dns_servers = [
@@ -26,26 +39,33 @@ def dnsQuery(site):
         except:
             pass
 
-    if len(ipList)>2:
-        with open('result.txt','a+') as f:
-            line = site + '\t' + 'CDN' + '\t' + ','.join(ipList)
+    if len(ipList)>limit:
+        with open('iscdner.txt','a+') as f:
+            line = '{}\tCDN\t{}'.format(site,','.join(ipList))
             f.write(line + '\n')
             print line
-    elif 0<len(ipList)<=2:
-        with open('result.txt','a+') as f:
-            line = site + '\t' + 'noCDN' + '\t' + ','.join(ipList)
+    elif 0<len(ipList)<=limit:
+        with open('iscdner.txt','a+') as f:
+            line = '{}\tnoCDN\t{}'.format(site,','.join(ipList))
             f.write(line + '\n')
             print line
+        with open('iscdnerIP.txt','a+') as f:
+            for ip in ipList:
+                if isIP(ip) and ip.strip() not in limitIP:
+                    f.write(ip.strip()+'\n')
+                    limitIP.append(ip.strip())
     else:
-        with open('result.txt','a+') as f:
+        with open('iscdner.txt','a+') as f:
             line = site + '\t' + 'error'
             f.write(line + '\n')
             print line
 
 if __name__ == '__main__':
-    open('result.txt','a+').truncate()
+    open('iscdner.txt','a+').truncate()
+    open('iscdnerIP.txt', 'a+').truncate()
     if os.path.isfile(sys.argv[1]):
         for line in open(sys.argv[1],'r+').readlines():
             dnsQuery(line.strip())
     else:
         dnsQuery(sys.argv[1].strip())
+
